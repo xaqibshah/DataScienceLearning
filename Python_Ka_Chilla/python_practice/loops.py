f1d5382fd1c70e5c199d3d0202dbe898fa8e6c96
Saqib Shah <saqibshah@live.com>
2023-10-08 01:42:34 +0500

    learned loop and condition statements

---
+++ Python_Ka_Chilla/python_practice/loops.py
@@ -0,0 +1,46 @@
+#conditional statements
+
+# >
+# <
+# >=
+# <=
+# ==
+# !=
+
+# x = 0
+# if x >  0:
+#     print('x is positive')
+# elif x < 0:
+#     print('x is negative')
+
+# else:
+#     print('x is Zero')
+
+foods = ['Dahi Bhallay', 'Biryani', 'Daal', 'Samosay', 'Shami', 'Palak Paneer']
+
+# for food in foods:
+#     print(food)
+
+# for i in range(len(foods)):
+#     print(foods[i])
+
+# i = 0
+
+# while i <= 10:
+#     print(i)
+#     i += 1
+
+# for letters in 'python':
+#     if letters == 'h':
+#         break
+#     print(letters)
+
+# for letters in 'python':
+#     if letters == 'h':
+#         continue
+#     print(letters)
+
+for letters in 'python':
+    if letters == 'h':
+        pass
+    print(letters)
