cde1ec85a2332a9fca1d1f6f4d6d416ca2ea70ba
Saqib Shah <saqibshah@live.com>
2023-10-07 23:59:18 +0500

    work with data stracture in python

---
+++ Python_Ka_Chilla/python_practice/data_structure_index.py
@@ -0,0 +1,38 @@
+# 01 List
+
+# food = ['Dahi Bhallay', 'Biryani', 'Daal', 'Samosay', 'Shami', 'Palak Paneer']
+
+
+# print(food[0])
+# print(food[-1])
+
+# food[0] = 'Chiken Puloe'
+
+# print(food[0])
+
+
+# 02 Tupple
+# coordinates = (4.21, 9.29)
+# print(coordinates)
+# print(coordinates[0])
+
+
+# 03 Set
+
+# fruit_set = {'Dahi bhallay', 'Biryani', "Dall"}  #we can not 
+# print(fruit_set)
+
+# fruit_set.add('Samosa')
+# print(fruit_set)
+
+
+# 04 Dictionary
+
+car = {
+    'brand': 'Ford',
+    'model': 'Mustang',
+    'year': 1964
+}
+print(car)
+car['year'] = 1998
+print(car['year'])
