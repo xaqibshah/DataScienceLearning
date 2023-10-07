32b1088ff4bbb810568483c252158a6508940e62
Saqib Shah <saqibshah@live.com>
2023-10-07 20:59:16 +0500

    start working with operators and variable

---
+++ Python_Ka_Chilla/python_practice/operators.py
@@ -0,0 +1,12 @@
+print(3+4)
+
+print("Learning python with Sir Ammar")
+print('learning python with codanics')
+
+print(''' This is multi line
+    string.
+      ''')
+
+a = 2
+b = 3
+print(a+b)
---
+++ Python_Ka_Chilla/python_practice/variables.py
@@ -0,0 +1,7 @@
+a = 5
+b = "Saqib"
+c = 5.5
+print(a, b, c)
+# print(type(a), type(b), type(c))
+if type(b) == str:
+    print("This is integer")
